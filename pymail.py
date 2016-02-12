ea3703212798a0c9dbbd11b801a1dd36160b8f19
dethmix <dethmix@users.noreply.github.com>
2016-02-12 09:47:02 +0200

    added html support

--- pymail.py
+++ pymail.py
@@ -1,13 +1,14 @@
-#!/usr/bin/pyhon
+#!/usr/bin/python
 
 import smtplib
 
 send = 'dmitriy.pavlov@ecommerce.com'
 recipient = 'dethmix@gmail.com'
 
-message = """ From: <dmitriy.pavlov@ecommerce.com>
-To:dethmix@gmail.com
+message = """From: <dmitriy.pavlov@ecommerce.com>
+To: <dethmix@gmail.com>
 Subject: 'Hello pymail'
+
 
 Test message from the python script
 """
