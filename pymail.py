f2a0c918ac5149bc77536c5e19860d3ca51770d9
dethmix <dethmix@users.noreply.github.com>
2016-02-12 09:24:18 +0200

    first script

---
+++ pymail.py
@@ -0,0 +1,18 @@
+#!/usr/bin/pyhon
+
+import smtplib
+
+send = 'dmitriy.pavlov@ecommerce.com'
+recipient = 'dethmix@gmail.com'
+
+message = """ From: <dmitriy.pavlov@ecommerce.com>
+To:dethmix@gmail.com
+Subject: 'Hello pymail'
+
+Test message from the python script
+"""
+
+smtpObj=smtplib.SMTP('localhost')
+smtpObj.sendmail(send, recipient, message)
+
+
